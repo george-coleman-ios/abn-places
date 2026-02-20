e240fa05993c3ea17fde310ec8718d4f1e0277f4
George <admin@colemanapps.com>
2026-02-20 21:18:45 +0100

    Add tests for placesActivityWithURL
    + add a small note to the README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # abn-places
+
+Things I would do if I had a lot more time:
+- PlacesViewController is a behemoth of a class. I wanted to write a test for showCoordinate (the method I added) but there is currently no test infrastructure in place and all business logic is written in the VC. To write tests for this class a significant refactor of the class is needed. This felt out of scope for this assignment. 
+- 
--- wikipedia-app/WikipediaUnitTests/Code/NSUserActivity+WMFExtensionsTest.m
+++ wikipedia-app/WikipediaUnitTests/Code/NSUserActivity+WMFExtensionsTest.m
@@ -45,4 +45,41 @@
                           @"https://en.wikipedia.org/w/index.php?search=dog&title=Special:Search&fulltext=1");
 }
 
+- (void)testPlacesWithArticleURL {
+    NSURL *url = [NSURL URLWithString:@"wikipedia://places?WMFArticleURL=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FAmsterdam"];
+    NSUserActivity *activity = [NSUserActivity wmf_activityForWikipediaScheme:url];
+    XCTAssertEqual(activity.wmf_type, WMFUserActivityTypePlaces);
+    XCTAssertEqualObjects(activity.webpageURL.absoluteString, @"https://en.wikipedia.org/wiki/Amsterdam");
+    XCTAssertNil(activity.userInfo[@"latitude"]);
+    XCTAssertNil(activity.userInfo[@"longitude"]);
+}
+
+- (void)testPlacesWithCoordinates {
+    NSURL *url = [NSURL URLWithString:@"wikipedia://places?latitude=52.3676&longitude=4.9041"];
+    NSUserActivity *activity = [NSUserActivity wmf_activityForWikipediaScheme:url];
+    XCTAssertEqual(activity.wmf_type, WMFUserActivityTypePlaces);
+    XCTAssertNil(activity.webpageURL);
+    XCTAssertEqualWithAccuracy([activity.userInfo[@"latitude"] doubleValue], 52.3676, 0.0001);
+    XCTAssertEqualWithAccuracy([activity.userInfo[@"longitude"] doubleValue], 4.9041, 0.0001);
+}
+
+- (void)testPlacesWithCoordinatesAndName {
+    NSURL *url = [NSURL URLWithString:@"wikipedia://places?latitude=52.3676&longitude=4.9041&name=Amsterdam"];
+    NSUserActivity *activity = [NSUserActivity wmf_activityForWikipediaScheme:url];
+    XCTAssertEqual(activity.wmf_type, WMFUserActivityTypePlaces);
+    XCTAssertNil(activity.webpageURL);
+    XCTAssertEqualWithAccuracy([activity.userInfo[@"latitude"] doubleValue], 52.3676, 0.0001);
+    XCTAssertEqualWithAccuracy([activity.userInfo[@"longitude"] doubleValue], 4.9041, 0.0001);
+    XCTAssertEqualObjects(activity.userInfo[@"name"], @"Amsterdam");
+}
+
+- (void)testPlacesWithNoParams {
+    NSURL *url = [NSURL URLWithString:@"wikipedia://places"];
+    NSUserActivity *activity = [NSUserActivity wmf_activityForWikipediaScheme:url];
+    XCTAssertEqual(activity.wmf_type, WMFUserActivityTypePlaces);
+    XCTAssertNil(activity.webpageURL);
+    XCTAssertNil(activity.userInfo[@"latitude"]);
+    XCTAssertNil(activity.userInfo[@"longitude"]);
+}
+
 @end
